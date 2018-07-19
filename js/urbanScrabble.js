9c63f3b91f40f7ad38ab12dce8864106750e37d3
grebwerd <grebwerd@gmail.com>
2018-07-18 22:19:24 -0600

    Can now get the definition of inputted term

--- js/urbanScrabble.js
+++ js/urbanScrabble.js
@@ -1,25 +1,39 @@
 
 
-const baseURL = "http://api.urbandictionary.com"
-const randomUrl = baseURL + "/v0/random";
+
+const baseApiURL = "http://api.urbandictionary.com";
+const apiURLDefine = baseApiURL + "/v0/define?term=";
+const randomUrl = baseApiURL + "/v0/random";
 
 function getRandomWord(){
     var resultElement = document.getElementById('getResult1');
+  
   resultElement.innerHTML = '';
     axios.get(randomUrl)
     .then(function(response){
+       console.log(response);
         resultElement.innerHTML = generateSuccessHTMLOutput(response)
     })
     .catch(err=>console.log(err));
 }
 
 
-function generateSuccessHTMLOutput(response) {
-    return  '<h4>Result</h4>' + 
-            '<h5>Status:</h5> ' + 
-            '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
-            '<h5>Headers:</h5>' + 
-            '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' + 
+function getWord(){
+    
+    var resultElement = document.getElementById('getResult1');
+  
+    resultElement.innerHTML = '';
+    var word = document.getElementById('defineWord').value
+      axios.get(apiURLDefine+word)
+      .then(function(response){
+         console.log(response);
+          resultElement.innerHTML = generateSuccessHTMLOutput(response, word)
+      })
+      .catch(err=>console.log(err));
+}
+
+function generateSuccessHTMLOutput(response, word) {
+    return  '<h4>Result for ' + word + ' </h4>' + 
             '<h5>Data:</h5>' + 
             '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>'; 
   }
